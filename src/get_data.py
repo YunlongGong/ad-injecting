52e21aac867bc4c8b8bfbc9359f7fc89aa7529b1
YunlongGong <yunlonggong@163.com>
2018-04-22 02:37:51 +0800

    remove not used code in get_data

--- src/get_data.py
+++ src/get_data.py
@@ -12,7 +12,3 @@ def get_data(url):
     content_raw = soup.article.get_text()
     content = "".join(content_raw.split())
     return content
-
-
-if __name__ == "__main__":
-    get_data('https://www.sohu.com/a/227962203_118792?_f=index_businessnews_0_2')
